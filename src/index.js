ab7e6dd94d408498a38d722ced4fdbd05262c858
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-03-01 14:31:42 +0530

    refactor: add close-modal class to close button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 
-// const createProjectDialog = document.querySelector('#create-project+dialog');
-// createProjectDialog.showModal();
-const createTODODialog = document.querySelector('#create-todo+dialog');
-createTODODialog.showModal();
+const createProjectDialog = document.querySelector('#create-project+dialog');
+createProjectDialog.showModal();
+// const createTODODialog = document.querySelector('#create-todo+dialog');
+// createTODODialog.showModal();
