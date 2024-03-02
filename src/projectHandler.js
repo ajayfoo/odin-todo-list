67bdb9c4793eb0ef2f80f65a9acb095f5f6b432f
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-03-02 16:12:59 +0530

    feat: Add project to list on creation

--- src/projectHandler.js
+++ src/projectHandler.js
@@ -1,11 +1,16 @@
 const projects = [];
 
+const projectSelectElement = document.querySelector("#create-todo-project-name");
 const createProject = (name) => {
     const getName = () => name;
     const TODOs = [];
     const addTODO = (todo) => {
         TODOs.push(todo);
     };
+    const projectOption = document.createElement('option');
+    projectOption.textContent = name;
+    projectOption.setAttribute('index', projects.length);
+    projectSelectElement.appendChild(projectOption);
     projects.push({ getName, addTODO });
     console.log(projects[projects.length - 1].getName());
 };
