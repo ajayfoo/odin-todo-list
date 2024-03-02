be91a011de344bf6524fee854ad194bfeae3bdeb
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-03-02 19:54:48 +0530

    fix: make forms submit without 'invalid field' error

--- src/projectFormHandler.js
+++ src/projectFormHandler.js
@@ -1,23 +1,12 @@
 import * as ProjectHandler from './projectHandler';
-import { closeCreateProjectDialog } from './modalHandler';
 
 const projectForm = document.querySelector('#create-project+dialog>form');
-const createProjectBtn = projectForm.querySelector('.create-form-buttons>button:first-child');
-const createProjectAndGoBtn = projectForm.querySelector('.create-form-buttons>button:last-child');
+const projectNameElement = projectForm.querySelector('#project-name');
 
 const setupEventListeners = () => {
-    const projectNameElement = projectForm.querySelector('#project-name');
-    createProjectBtn.addEventListener('click', () => {
-        if (!projectForm.checkValidity()) return;
+    projectForm.addEventListener('submit', () => {
         ProjectHandler.createProject(projectNameElement.value);
-        closeCreateProjectDialog();
-        projectNameElement.value = '';
-    });
-    createProjectAndGoBtn.addEventListener('click', () => {
-        if (!projectForm.checkValidity()) return;
-        ProjectHandler.createProject(projectNameElement.value);
-        closeCreateProjectDialog();
-        projectNameElement.value = '';
+        projectForm.reset();
     });
 };
 
--- src/todoFormHandler.js
+++ src/todoFormHandler.js
@@ -1,27 +1,14 @@
-import { closeCreateTODODialog } from './modalHandler';
 import * as TodoHandler from './todoHandler';
 
 const todoForm = document.querySelector('#create-todo+dialog>form');
-
-const createTODOBtn = todoForm.querySelector('.create-form-buttons>button:first-child');
-const createTODOAndGoBtn = todoForm.querySelector('.create-form-buttons>button:last-child');
 const todoNameEle = document.querySelector('#todo-name');
 const projectSelectEle = document.querySelector('#create-todo-project-name');
 const todoPriorityEle = document.querySelector('#create-todo-priority');
 const todoDescriptionEle = document.querySelector('#create-todo-description');
 const todoDueDateEle = document.querySelector('#create-todo-due-date');
 
-const resetAllFields = () => {
-    todoNameEle.value = '';
-    projectSelectEle.selectedIndex = 0;
-    todoPriorityEle.selectedIndex = 0;
-    todoDescriptionEle.value = '';
-    todoDueDateEle.value = null;
-};
-
 const setupEventListeners = () => {
-    createTODOBtn.addEventListener('click', () => {
-        if (!todoForm.checkValidity()) return;
+    todoForm.addEventListener('submit', () => {
         TodoHandler.createTODO(
             todoNameEle.value,
             projectSelectEle.value,
@@ -29,21 +16,7 @@ const setupEventListeners = () => {
             todoDescriptionEle.value,
             todoDueDateEle.value
         );
-        closeCreateTODODialog();
-        resetAllFields();
-    });
-    createTODOAndGoBtn.addEventListener('click', () => {
-        if (!todoForm.checkValidity()) return;
-        ProjectHandler.createProject(projectNameElement.value);
-        TodoHandler.createTODO(
-            todoNameEle.value,
-            projectSelectEle.value,
-            todoPriorityEle.value,
-            todoDescriptionEle.value,
-            todoDueDateEle.value
-        );
-        closeCreateTODODialog();
-        resetAllFields();
+        todoForm.reset();
     });
 };
 
