b85e25c0c57d700fbcd0b92e0cf123048219188d
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-03-03 11:14:10 +0530

    fix: fix description toggle error when clicked on .twin or .due-date

--- src/todoComponent.js
+++ src/todoComponent.js
@@ -5,12 +5,28 @@ const toggleDescription = (todo) => {
     const computedStyleDisplay = window.getComputedStyle(description).display;
     description.style.display = computedStyleDisplay === 'none' ? 'block' : 'none';
 }
+const twinElements = Array.from(todoHeaderElements)
+    .map(header => header.querySelector('.twin'));
+const dueDateElements = Array.from(todoHeaderElements)
+    .map(header => header.querySelector('.due-date'));
+
 const setupEventListeners = () => {
     todoHeaderElements.forEach(header => {
         header.addEventListener('click', (event) => {
+            event.stopPropagation();
             toggleDescription(event.target.parentElement);
         });
     });
-}
+    twinElements.forEach(twin => {
+        twin.addEventListener('click', (event) => {
+            event.stopPropagation();
+        })
+    });
+    dueDateElements.forEach(dueDateEle => {
+        dueDateEle.addEventListener('click', (event) => {
+            event.stopPropagation();
+        })
+    });
+};
 
 export { setupEventListeners };
