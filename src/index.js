69444e7225189e47f28bc6b3d2de4c0a7547f572
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-03-04 15:29:17 +0530

    feat: add #all-projects

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import './style.css';
 import * as ModalHandler from './modalHandler';
 import * as ProjectFromHandler from './projectFormHandler';
 import * as TodoFormHandler from './todoFormHandler';
-import * as TodoComponent from './todoComponent';
 import * as ProjectSelector from './projectSelector';
 
 ModalHandler.setupEventListeners();
 ProjectFromHandler.setupEventListeners();
 TodoFormHandler.setupEventListeners();
-TodoComponent.setupEventListeners();
 ProjectSelector.setupEventListeners();
--- src/todoComponent.js
+++ src/todoComponent.js
@@ -1,63 +1,10 @@
 import * as ContainerComponent from './containerComponent';
-const todoHeaderElements = Array.from(
-    document.querySelectorAll("body > .container > .todo > header")
-);
+
 const toggleDescription = (todo) => {
     const description = todo.querySelector('.description');
     const computedStyleDisplay = window.getComputedStyle(description).display;
     description.style.display = computedStyleDisplay === 'none' ? 'block' : 'none';
 }
-
-const twinElements = todoHeaderElements.map(header => header.querySelector('.twin'));
-const dueDateElements = todoHeaderElements.map(header => header.querySelector('.due-date'));
-const showMoreElements = todoHeaderElements.map(header => header.parentElement
-    .querySelector('.description')
-    .querySelector('.show-more')
-);
-const showLessElements = todoHeaderElements.map(header => header.parentElement
-    .querySelector('.description')
-    .querySelector('.show-less')
-);
-
-const setupEventListeners = () => {
-    todoHeaderElements.forEach(header => {
-        header.addEventListener('click', (event) => {
-            event.stopPropagation();
-            toggleDescription(event.target.parentElement);
-        });
-    });
-    twinElements.forEach(twin => {
-        twin.addEventListener('click', (event) => {
-            event.stopPropagation();
-        });
-    });
-    dueDateElements.forEach(dueDateEle => {
-        dueDateEle.addEventListener('click', (event) => {
-            event.stopPropagation();
-        });
-    });
-    console.log(showMoreElements);
-    showMoreElements.forEach(showMore => {
-        showMore.addEventListener('click', () => {
-            const parent = showMore.parentElement;
-            const restElem = parent.querySelector('.rest');
-            const showLess = parent.querySelector('.show-less');
-            restElem.style.display = 'inline';
-            showMore.style.display = 'none';
-            showLess.style.display = 'inline';
-        });
-    });
-    showLessElements.forEach(showLess => {
-        showLess.addEventListener('click', () => {
-            const parent = showLess.parentElement;
-            const restElem = parent.querySelector('.rest');
-            const showMore = parent.querySelector('.show-more');
-            restElem.style.display = 'none';
-            showLess.style.display = 'none';
-            showMore.style.display = 'inline';
-        });
-    });
-};
 
 const getNewHeaderComponent = (todo) => {
     const priority = document.createElement('span');
@@ -146,4 +93,4 @@ const createTODOComponent = (projectIndex, todo) => {
     ContainerComponent.addTODOComponent(projectIndex, getNewTODOComponent(todo));
 };
 
-export { setupEventListeners, createTODOComponent };
+export { createTODOComponent };
